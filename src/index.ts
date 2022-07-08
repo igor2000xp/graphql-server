c58904cad3500ad27098fa585068e4c3577d3ce1
igor2000xp <igorring@gmail.com>
2022-07-08 04:57:45 +0300

    feat: change schema, now schema is Book from docs

--- src/index.ts
+++ src/index.ts
@@ -2,33 +2,61 @@ import { ApolloServer, gql } from 'apollo-server-express';
 import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
 import express from 'express';
 import http from 'http';
+import { SERVER_PORT } from './constants/constants';
+import * as dotenv from 'dotenv';
+import { resolve } from 'path';
+import { cwd } from 'process';
 
+dotenv.config({ path: resolve(cwd(), '.env') });
+const serverPort = Number(process.env.HTTP_PORT) || SERVER_PORT;
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
 const typeDefs = gql`
-  type Query {
-    hello: String
-  }
+
+type Book {
+  title: String
+  author: String
+}
+
+# The "Query" type is special: it lists all of the available queries that
+# clients can execute, along with the return type for each. In this
+# case, the "books" query returns an array of zero or more Books (defined above).
+type Query {
+  books: [Book]
+}
 `;
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+];
 
 const resolvers = {
   Query: {
-    hello() {
-      return 'world';
-    },
+    books: () => books,
   },
 };
 
 async function listen(port: number) {
   const app = express();
   const httpServer = http.createServer(app);
-
-  const server = new ApolloServer({
+  const apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
-  await server.start();
 
-  server.applyMiddleware({ app });
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
 
   return new Promise((resolve, reject) => {
     httpServer.listen(port).once('listening', resolve).once('error', reject)
@@ -37,34 +65,11 @@ async function listen(port: number) {
 
 async function main() {
   try {
-    await listen(4000);
-    console.log('🚀 Server is ready at http://localhost:4000/graphql');
+    await listen(serverPort);
+    console.log(`🚀 Server started at http://localhost:${serverPort}/graphql`);
   } catch (err) {
-    console.error('💀 Error starting the node server', err);
+    console.error('💀 Error starting the apollo server', err);
   }
 }
 
 void main();
-
-
-// const { ApolloServer, gql } = require('apollo-server');
-//
-// // A schema is a collection of type definitions (hence "typeDefs")
-// // that together define the "shape" of queries that are executed against
-// // your data.
-// const typeDefs = gql`
-//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
-//
-//   # This "Book" type defines the queryable fields for every book in our data source.
-//   type Book {
-//     title: String
-//     author: String
-//   }
-//
-//   # The "Query" type is special: it lists all of the available queries that
-//   # clients can execute, along with the return type for each. In this
-//   # case, the "books" query returns an array of zero or more Books (defined above).
-//   type Query {
-//     books: [Book]
-//   }
-// `;
