a40ba141d1128007d258b0efcceff8c5561a9842
igor2000xp <igorring@gmail.com>
2022-07-08 03:57:40 +0300

    init: reinit server

---
+++ src/index.ts
@@ -0,0 +1,70 @@
+import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServerPluginDrainHttpServer } from 'apollo-server-core';
+import express from 'express';
+import http from 'http';
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello() {
+      return 'world';
+    },
+  },
+};
+
+async function listen(port: number) {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+  await server.start();
+
+  server.applyMiddleware({ app });
+
+  return new Promise((resolve, reject) => {
+    httpServer.listen(port).once('listening', resolve).once('error', reject)
+  });
+}
+
+async function main() {
+  try {
+    await listen(4000);
+    console.log('🚀 Server is ready at http://localhost:4000/graphql');
+  } catch (err) {
+    console.error('💀 Error starting the node server', err);
+  }
+}
+
+void main();
+
+
+// const { ApolloServer, gql } = require('apollo-server');
+//
+// // A schema is a collection of type definitions (hence "typeDefs")
+// // that together define the "shape" of queries that are executed against
+// // your data.
+// const typeDefs = gql`
+//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+//
+//   # This "Book" type defines the queryable fields for every book in our data source.
+//   type Book {
+//     title: String
+//     author: String
+//   }
+//
+//   # The "Query" type is special: it lists all of the available queries that
+//   # clients can execute, along with the return type for each. In this
+//   # case, the "books" query returns an array of zero or more Books (defined above).
+//   type Query {
+//     books: [Book]
+//   }
+// `;
